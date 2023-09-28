792337952fa509cdce1b6f49b991c8a065f55586
Lana gonçalves <121570997+lanaclaudias@users.noreply.github.com>
2023-09-28 11:58:28 -0300

    atualizações

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', projeto: 'Projeto Ninho' });
+  console.log(req.query)
+  res.render('index', req.query);
 });
 
 module.exports = router;
